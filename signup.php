7ec80d2e64c5da90cb0d6cf2b9d335c121eb8078
bk1405bgl <bk1405bgl@gmail.com>
2023-10-26 09:01:46 +0200

    css cleaned up

--- signup.php
+++ signup.php
@@ -1,16 +1,47 @@
+<!DOCTYPE html>
+<html lang="de">
 
+<head>
+    <meta charset="utf-8">
+    <title>Chemperator</title>
+    <meta name="description" content="Chemperator ERP System for chemical industry businesses">
+    <meta name="author" content="Bilal Kuzey">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
 
-<form action="f_signup.php">
-    <div class="container">
-        <h1>Registrieren</h1>
-        <p>Bitte füllen Sie die untenstehenden Felder aus.</p>
-        <hr>
-        <label for="email">E-Mail</label>
-        <input type="text" placeholder="E-Mail Adresse eingeben" name="email" id="email" required>
-        <label for="password">Passwort</label>
-        <input type="password" placeholder="Passwort eingeben" name="password" id="password" required>
-        <label for="r_password">Passwort wiederholen</label>
-        <input type="password" placeholder="Passwort wiederholen" name="r_password" id="r_password" required>
-        <button type="submit">Registrieren</button>
-    </div>
-</form>
+<body>
+<div>
+    <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="signin.php">Einloggen</a></li>
+            <li><a href="signup.php">Registrieren</a></li>
+        </ul>
+    </nav>
+    <main>
+        <form action="f_signup.php">
+            <div class="container">
+                <h1>Registrieren</h1>
+                <p>Bitte füllen Sie die untenstehenden Felder aus.</p>
+                <hr>
+                <label for="email">E-Mail</label>
+                <input type="text" placeholder="E-Mail Adresse eingeben" name="email" id="email" required>
+                <label for="password">Passwort</label>
+                <input type="password" placeholder="Passwort eingeben" name="password" id="password" required>
+                <label for="r_password">Passwort wiederholen</label>
+                <input type="password" placeholder="Passwort wiederholen" name="r_password" id="r_password" required>
+                <button type="submit">Registrieren</button>
+            </div>
+        </form>
+    </main>
+    <footer>
+        <div>
+            <p>Chemperator &copy;<?= date("Y");?></p>
+        </div>
+    </footer>
+</div>
+</body>
+
+</html>
