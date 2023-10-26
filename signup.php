84dbd6312b099c0f4fc10cc25f730496f5429221
bk1405bgl <bk1405bgl@gmail.com>
2023-10-26 08:38:28 +0200

    signup-form created

--- signup.php
+++ signup.php
@@ -1,1 +1,16 @@
-<?php
+
+
+<form action="f_signup.php">
+    <div class="container">
+        <h1>Registrieren</h1>
+        <p>Bitte füllen Sie die untenstehenden Felder aus.</p>
+        <hr>
+        <label for="email">E-Mail</label>
+        <input type="text" placeholder="E-Mail Adresse eingeben" name="email" id="email" required>
+        <label for="password">Passwort</label>
+        <input type="password" placeholder="Passwort eingeben" name="password" id="password" required>
+        <label for="r_password">Passwort wiederholen</label>
+        <input type="password" placeholder="Passwort wiederholen" name="r_password" id="r_password" required>
+        <button type="submit">Registrieren</button>
+    </div>
+</form>
