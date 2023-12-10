f0fdda076525467220cc48969e166137147d0dc3
Bilal Kuzey <bk1405bgl@gmail.com>
2023-12-10 19:27:11 +0100

    ins grid system umgestellt

--- includes/02.inc.php
+++ includes/02.inc.php
@@ -1,17 +1,19 @@
 <body>
-    <nav>
-        <ul>
-            <li><a href="/">Home</a></li>
-            <?php
-            if(isset($_SESSION["userid"])) {
-                ?>
-                <li><a href="userpage.php"><?php echo $_SESSION["useruid"]; ?></a></li>
-                <li><a href="includes/logout.inc.php">Logout</a></li>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
                 <?php
-            } else {
-                ?>
-                <li><button type="button" onclick="openModal('modal1')">Registrieren</button></li>
-                <li><button type="button" onclick="openModal('modal2')">Einloggen</button></li>
-            <?php } ?>
-        </ul>
-    </nav>
+                if(isset($_SESSION["userid"])) {
+                    ?>
+                    <li><a href="userpage.php"><?php echo $_SESSION["useruid"]; ?></a></li>
+                    <li><a href="includes/logout.inc.php">Logout</a></li>
+                    <?php
+                } else {
+                    ?>
+                    <li><button type="button" onclick="openModal('modal1')">Registrieren</button></li>
+                    <li><button type="button" onclick="openModal('modal2')">Einloggen</button></li>
+                <?php } ?>
+            </ul>
+        </nav>
+    </header>
--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 ?>
 <?php include 'includes/00.inc.php'; ?>
     <title>Chemperator</title>
-<?php include 'includes/01.inc.php'; ?>
-<?php include 'includes/02.inc.php'; ?>
+    <?php include 'includes/01.inc.php'; ?>
+    <?php include 'includes/02.inc.php'; ?>
     <?php
         if(isset($_GET["error"])) {
             $error = $_GET["error"];
@@ -27,50 +27,50 @@
             }
         }
     ?>
-        <main>
-            <h1>Chemperator</h1>
-            <p>Chemperator ist eine ERP-Software, speziell für Firmen in der Chemiebranche entwickelt.</p>
-            <h2>Kundenverwaltung</h2>
-            <p>Verwalten Sie Ihre Kunden und ziehen Sie Schlüsse aus Statistiken und Auswertungen. Bestimmen Sie Top-Kunden, gewähren Sie individuelle Rabatte und vieles mehr.</p>
-            <h2>Artikelverwaltung</h2>
-            <p>Artikel Auflistung, Detailbearbeitung, Preiskalkulation inklusiver direkter Abstammung aus den Rezepturen.</p>
-            <h2>Rezepturverwaltung</h2>
-            <p>Rezepturen bestehen aus Rohstoffen und können mehreren Produkten zugewiesen werden. Eine Rezeptur kann auch nur verdünnt einem Produkt zugewiesen werden.</p>
-            <h2>Auftragsverwaltung</h2>
-            <p>Rechnungen, Gutschriften, Lieferscheine, uvm. können hier verwaltet werden. Statistiken und Auswertungen können in beliebigen Konstellationen erstellt werden.</p>
-            <div id="modal1" class="modal">
-                <div class="modal-content">
-                    <span class="close" onclick="closeModal('modal1')"><i class="fa-solid fa-circle-xmark"></i></span>
-                    <form action="includes/signup.inc.php" method="post" autocomplete="off">
-                        <div class="container">
-                            <label>Username</label>
-                            <input type="text" placeholder="username" name="uid">
-                            <label>Passwort</label>
-                            <input type="password" placeholder="********" name="pwd" required>
-                            <label>Passwort wiederholen</label>
-                            <input type="password" placeholder="********" name="pwd_repeat" required>
-                            <label>E-Mail</label>
-                            <input type="email" placeholder="max@mustermann.de" name="email" required>
-                            <button type="submit" name="submit" title="Registrieren"><i class="fa-solid fa-user-plus">&nbsp;</i>Registrieren</button>
-                            <button type="reset" name="reset" title="Zurücksetzen"><i class="fa-solid fa-delete-left"></i></button>
-                        </div>
-                    </form>
-                </div>
+    <main>
+        <h1>Chemperator</h1>
+        <p>Chemperator ist eine ERP-Software, speziell für Firmen in der Chemiebranche entwickelt.</p>
+        <h2>Kundenverwaltung</h2>
+        <p>Verwalten Sie Ihre Kunden und ziehen Sie Schlüsse aus Statistiken und Auswertungen. Bestimmen Sie Top-Kunden, gewähren Sie individuelle Rabatte und vieles mehr.</p>
+        <h2>Artikelverwaltung</h2>
+        <p>Artikel Auflistung, Detailbearbeitung, Preiskalkulation inklusiver direkter Abstammung aus den Rezepturen.</p>
+        <h2>Rezepturverwaltung</h2>
+        <p>Rezepturen bestehen aus Rohstoffen und können mehreren Produkten zugewiesen werden. Eine Rezeptur kann auch nur verdünnt einem Produkt zugewiesen werden.</p>
+        <h2>Auftragsverwaltung</h2>
+        <p>Rechnungen, Gutschriften, Lieferscheine, uvm. können hier verwaltet werden. Statistiken und Auswertungen können in beliebigen Konstellationen erstellt werden.</p>
+        <div id="modal1" class="modal">
+            <div class="modal-content">
+                <span class="close" onclick="closeModal('modal1')"><i class="fa-solid fa-circle-xmark"></i></span>
+                <form action="includes/signup.inc.php" method="post" autocomplete="off">
+                    <div class="container">
+                        <label>Username</label>
+                        <input type="text" placeholder="username" name="uid">
+                        <label>Passwort</label>
+                        <input type="password" placeholder="********" name="pwd" required>
+                        <label>Passwort wiederholen</label>
+                        <input type="password" placeholder="********" name="pwd_repeat" required>
+                        <label>E-Mail</label>
+                        <input type="email" placeholder="max@mustermann.de" name="email" required>
+                        <button type="submit" name="submit" title="Registrieren"><i class="fa-solid fa-user-plus">&nbsp;</i>Registrieren</button>
+                        <button type="reset" name="reset" title="Zurücksetzen"><i class="fa-solid fa-delete-left"></i></button>
+                    </div>
+                </form>
             </div>
-            <div id="modal2" class="modal">
-                <div class="modal-content">
-                    <span class="close" onclick="closeModal('modal2')"><i class="fa-solid fa-circle-xmark"></i></span>
-                    <form action="includes/login.inc.php" method="post" autocomplete="off">
-                        <div class="container">
-                            <label>Username oder E-Mail Adresse</label>
-                            <input type="text" placeholder="Username / E-Mail Adresse eingeben" name="uid" required>
-                            <label>Passwort</label>
-                            <input type="password" placeholder="Passwort eingeben" name="pwd" required>
-                            <button type="submit" name="submit" title="Einloggen"><i class="fa-solid fa-right-to-bracket">&nbsp;</i>Einloggen</button>
-                            <button type="reset" name="reset" title="Zurücksetzen"><i class="fa-solid fa-delete-left"></i></button>
-                        </div>
-                    </form>
-                </div>
+        </div>
+        <div id="modal2" class="modal">
+            <div class="modal-content">
+                <span class="close" onclick="closeModal('modal2')"><i class="fa-solid fa-circle-xmark"></i></span>
+                <form action="includes/login.inc.php" method="post" autocomplete="off">
+                    <div class="container">
+                        <label>Username oder E-Mail Adresse</label>
+                        <input type="text" placeholder="Username / E-Mail Adresse eingeben" name="uid" required>
+                        <label>Passwort</label>
+                        <input type="password" placeholder="Passwort eingeben" name="pwd" required>
+                        <button type="submit" name="submit" title="Einloggen"><i class="fa-solid fa-right-to-bracket">&nbsp;</i>Einloggen</button>
+                        <button type="reset" name="reset" title="Zurücksetzen"><i class="fa-solid fa-delete-left"></i></button>
+                    </div>
+                </form>
             </div>
-        </main>
+        </div>
+    </main>
 <?php include 'includes/99.inc.php'; ?>
